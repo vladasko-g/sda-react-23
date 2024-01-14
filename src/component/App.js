965d6ac51e1f77c7e8ab91265c7708a0310be67a
vladasko-g <vladasko@protonmail.com>
2024-01-14 10:24:18 +0200

    7

--- src/component/App.js
+++ src/component/App.js
@@ -1,38 +1,5 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import { ThemeProvider } from "../theme";
-
-import { ContentMain } from "./ContentMain";
-import { PageRoot } from "./PageRoot";
-import { ContentContacts } from "./ContentContacts";
-import { ContentAbout } from "./ContentAbout";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <PageRoot />,
-    children: [
-      {
-        path: "/",
-        element: <ContentMain />,
-      },
-      {
-        path: "/about",
-        element: <ContentAbout />,
-      },
-      {
-        path: "/contact/:type",
-        element: <ContentContacts />,
-      },
-    ],
-  },
-]);
 
 export const App = () => {
-  return (
-    <ThemeProvider>
-      <RouterProvider router={router} />
-    </ThemeProvider>
-  );
+  return <div>App is running</div>;
 };
--- src/component/Cards.js
+++
@@ -1,9 +0,0 @@
-import React, { memo } from "react";
-
-import { Card } from "./Card";
-
-export const Cards = memo(({ data }) => {
-  return data.map(item => {
-    return <Card key={item.id} title={item.title} />;
-  });
-});
